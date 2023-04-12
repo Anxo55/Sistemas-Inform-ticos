45f75be290717a211b2dc180fb00e012832e3fb9
Anxo Campos <anxo.campos260204@gmail.com>
2023-04-12 18:13:22 +0200

    all

--- 3ªEV/Spring Boot/rickandmorty/src/main/java/com/cebem/rickandmorty/controllers/rickController.java
+++ 3ªEV/Spring Boot/rickandmorty/src/main/java/com/cebem/rickandmorty/controllers/rickController.java
@@ -1,7 +1,11 @@
 package com.cebem.rickandmorty.controllers;
 
+import java.io.IOException;
+import java.lang.ProcessHandle.Info;
 import java.text.MessageFormat;
+import java.util.Map;
 
+import org.apache.logging.log4j.message.Message;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +16,6 @@ import com.cebem.rickandmorty.utils.Utils;
 
 @RestController
 public class rickController {
-    // http://127.0.0.1/
-    // http://localhost/
-    // http://anxo.com/
     @GetMapping("/")
     public String saluda() {
         return "Bienvenid@ a mi api rest de rickAndMorty";
@@ -22,30 +23,37 @@ public class rickController {
 
     @GetMapping("/random")
     public String random() {
-        return Math.round(Math.random()*10) + "";
+        return Math.round(Math.random() * 10) + " ";
     }
 
-    //./mvnw spring-boot:run
-    //http://localhost:8080/palindrome/ana
-    @GetMapping("/palindromo/{word}")
+    @GetMapping("/palindrome/{word}")
     public String palindrome(@PathVariable String word) {
-        return Utils.isPalindrome(word) ?
-            "Si es palindromo" : "No es un palíndromo";
-        }
-        
-        //http://localhost:8000/add?n1=2&n2=4
-        @GetMapping("/add")
-        public String add(  @RequestParam String n1,
-                            @RequestParam String n2) {
-        float resultado = Float.parseFloat(n1) + Float.parseFloat(n2);
-        Object params[] = {n1,n2, resultado};
-        return MessageFormat.format(  "La suma de {0} mas {1} es igual a {2}", params);
-
-        }
-
-        @PostMapping("saveOnDisk")
-        public String saveOnDisk() {
-
-        }
-        
+        return Utils.isPalindrome(word) ? "Si es palíndromo" : "No es un palindromo";
     }
+
+    @GetMapping("/add")
+    public String add(@RequestParam String n1, @RequestParam String n2) {
+        float resultado = Float.parseFloat(n1) + Float.parseFloat(n2);
+        Object params[] = { n1, n2, resultado };
+        return MessageFormat.format("la suma de {0} mas {1} es igual a {2}", params);
+    }
+
+    @PostMapping("/saveOnDisk")
+    public String saveOnDisk(@RequestParam Map<String, String> body) {
+        String name= body.get("name");
+        String price= body.get("price");
+
+
+        String info= name+" - "+ price+" \n";
+
+        try {
+            Utils.writeOnDisk("datos.txt", info);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            return "Error al intentar escribir en el fichero";
+        }        
+
+        return "Gracias por enviar el formulario, los datos se han guardado en el servidor";
+    }
+
+}
--- 3ªEV/Spring Boot/rickandmorty/src/main/java/com/cebem/rickandmorty/utils/Utils.java
+++ 3ªEV/Spring Boot/rickandmorty/src/main/java/com/cebem/rickandmorty/utils/Utils.java
@@ -1,11 +1,25 @@
 package com.cebem.rickandmorty.utils;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Utils {
-    public static boolean isPalindrome(String word){
-        String inverseWord = new StringBuilder(word)
-        .reverse()
-        .toString();
+    public static boolean isPalindrome(String word) {
+        String inverseWord = new StringBuilder(word).reverse().toString();
+        // String inverseWord = sb.reverse().toString();
         return inverseWord.equalsIgnoreCase(word);
-       
-}
+
+    }
+
+    public static void writeOnDisk(String fileName,String info) throws IOException{
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(fileName, true);
+            fw.write(info);
+        }  finally{
+            if(fw!=null) fw.close();
+
+        }
+
+    }
 }
